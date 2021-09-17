3a2272fc217650c061fc7e0cdc500d3dec36b25e
Pawan Kumar <69069220+pawan-06@users.noreply.github.com>
2021-09-17 11:33:55 +0530

    Create Assignment6.java

---
+++ Java_Fundamental/Array/Assignment6.java
@@ -0,0 +1,19 @@
+//Write a program to initialize an array and print them in a sorted order.
+
+import java.util.Arrays;
+
+public class Assignment6 {
+
+	public static void main(String[] args) {
+		
+		int[] array = {19, 14, 20, 18, 2};
+		
+		Arrays.sort(array);												
+		
+		System.out.print("Sorted Array : ");
+		for(int element : array) {
+			System.out.print(element + " ");
+		}
+	}
+
+}
