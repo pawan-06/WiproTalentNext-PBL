2f4f9b1734401e0db08ba59aa510e2c785aeaf0f
Pawan Kumar <69069220+pawan-06@users.noreply.github.com>
2021-09-17 10:45:29 +0530

    Update and rename Java_Fundamental\Language_Basics to Java_Fundamental/Language_Basic/Assignment1.java

---
+++ Java_Fundamental/Language_Basic/Assignment1.java
@@ -0,0 +1,15 @@
+/*Write a Program that accepts two Strings as command line arguments and generate the output in the required format. 
+Example1)
+If the two command line arguments are Wipro and Bangalore then the output generated should be Wipro Technologies Bangalore.*/
+
+
+
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(args[0]+" "+"Technolgies"+" "+args[1]);
+	}
+
+}
